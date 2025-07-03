ffbddf4d630215807fb521636bfcc71be3223bc5
Santiago Carmuega <santiago@carmuega.me>
2025-07-03 09:52:05 -0300

    feat(cardano): support adhoc plutus witnesses

--- crates/tx3-lang/src/cardano.rs
+++ crates/tx3-lang/src/cardano.rs
@@ -284,10 +284,127 @@ impl IntoLower for StakeDelegationCertificate {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum PlutusWitnessField {
+    Version(DataExpr, Span),
+    Script(DataExpr, Span),
+}
+
+impl IntoLower for PlutusWitnessField {
+    type Output = (String, ir::Expression);
+
+    fn into_lower(
+        &self,
+        ctx: &crate::lowering::Context,
+    ) -> Result<Self::Output, crate::lowering::Error> {
+        match self {
+            PlutusWitnessField::Version(x, _) => Ok(("version".to_string(), x.into_lower(ctx)?)),
+            PlutusWitnessField::Script(x, _) => Ok(("script".to_string(), x.into_lower(ctx)?)),
+        }
+    }
+}
+
+impl AstNode for PlutusWitnessField {
+    const RULE: Rule = Rule::cardano_plutus_witness_field;
+
+    fn parse(pair: Pair<Rule>) -> Result<Self, Error> {
+        let span = pair.as_span().into();
+
+        match pair.as_rule() {
+            Rule::cardano_plutus_witness_version => {
+                Ok(PlutusWitnessField::Version(DataExpr::parse(pair)?, span))
+            }
+            Rule::cardano_plutus_witness_script => {
+                Ok(PlutusWitnessField::Script(DataExpr::parse(pair)?, span))
+            }
+            x => unreachable!("Unexpected rule in cardano_plutus_witness_field: {:?}", x),
+        }
+    }
+
+    fn span(&self) -> &Span {
+        match self {
+            PlutusWitnessField::Version(_, span) => span,
+            PlutusWitnessField::Script(_, span) => span,
+        }
+    }
+}
+
+impl Analyzable for PlutusWitnessField {
+    fn analyze(&mut self, parent: Option<Rc<Scope>>) -> AnalyzeReport {
+        match self {
+            PlutusWitnessField::Version(x, _) => x.analyze(parent),
+            PlutusWitnessField::Script(x, _) => x.analyze(parent),
+        }
+    }
+
+    fn is_resolved(&self) -> bool {
+        match self {
+            PlutusWitnessField::Version(x, _) => x.is_resolved(),
+            PlutusWitnessField::Script(x, _) => x.is_resolved(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct PlutusWitnessBlock {
+    pub fields: Vec<PlutusWitnessField>,
+    pub span: Span,
+}
+
+impl AstNode for PlutusWitnessBlock {
+    const RULE: Rule = Rule::cardano_plutus_witness_block;
+
+    fn parse(pair: Pair<Rule>) -> Result<Self, Error> {
+        let span = pair.as_span().into();
+        let inner = pair.into_inner();
+
+        let fields = inner
+            .map(|x| PlutusWitnessField::parse(x))
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(PlutusWitnessBlock { fields, span })
+    }
+
+    fn span(&self) -> &Span {
+        &self.span
+    }
+}
+
+impl Analyzable for PlutusWitnessBlock {
+    fn analyze(&mut self, parent: Option<Rc<Scope>>) -> AnalyzeReport {
+        self.fields.analyze(parent)
+    }
+
+    fn is_resolved(&self) -> bool {
+        self.fields.is_resolved()
+    }
+}
+
+impl IntoLower for PlutusWitnessBlock {
+    type Output = ir::AdHocDirective;
+
+    fn into_lower(
+        &self,
+        ctx: &crate::lowering::Context,
+    ) -> Result<Self::Output, crate::lowering::Error> {
+        let data = self
+            .fields
+            .iter()
+            .map(|x| x.into_lower(ctx))
+            .collect::<Result<_, _>>()?;
+
+        Ok(ir::AdHocDirective {
+            name: "plutus_witness".to_string(),
+            data,
+        })
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub enum CardanoBlock {
     VoteDelegationCertificate(VoteDelegationCertificate),
     StakeDelegationCertificate(StakeDelegationCertificate),
     Withdrawal(WithdrawalBlock),
+    PlutusWitness(PlutusWitnessBlock),
 }
 
 impl AstNode for CardanoBlock {
@@ -307,6 +424,9 @@ impl AstNode for CardanoBlock {
             Rule::cardano_withdrawal_block => {
                 Ok(CardanoBlock::Withdrawal(WithdrawalBlock::parse(item)?))
             }
+            Rule::cardano_plutus_witness_block => Ok(CardanoBlock::PlutusWitness(
+                PlutusWitnessBlock::parse(item)?,
+            )),
             x => unreachable!("Unexpected rule in cardano_block: {:?}", x),
         }
     }
@@ -316,6 +436,7 @@ impl AstNode for CardanoBlock {
             CardanoBlock::VoteDelegationCertificate(x) => x.span(),
             CardanoBlock::StakeDelegationCertificate(x) => x.span(),
             CardanoBlock::Withdrawal(x) => x.span(),
+            CardanoBlock::PlutusWitness(x) => x.span(),
         }
     }
 }
@@ -326,6 +447,7 @@ impl Analyzable for CardanoBlock {
             CardanoBlock::VoteDelegationCertificate(x) => x.analyze(parent),
             CardanoBlock::StakeDelegationCertificate(x) => x.analyze(parent),
             CardanoBlock::Withdrawal(x) => x.analyze(parent),
+            CardanoBlock::PlutusWitness(x) => x.analyze(parent),
         }
     }
 
@@ -334,6 +456,7 @@ impl Analyzable for CardanoBlock {
             CardanoBlock::VoteDelegationCertificate(x) => x.is_resolved(),
             CardanoBlock::StakeDelegationCertificate(x) => x.is_resolved(),
             CardanoBlock::Withdrawal(x) => x.is_resolved(),
+            CardanoBlock::PlutusWitness(x) => x.is_resolved(),
         }
     }
 }
@@ -349,6 +472,48 @@ impl IntoLower for CardanoBlock {
             CardanoBlock::VoteDelegationCertificate(x) => x.into_lower(ctx),
             CardanoBlock::StakeDelegationCertificate(x) => x.into_lower(ctx),
             CardanoBlock::Withdrawal(x) => x.into_lower(ctx),
+            CardanoBlock::PlutusWitness(x) => x.into_lower(ctx),
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ast::*;
+    use pest::Parser;
+
+    macro_rules! input_to_ast_check {
+        ($ast:ty, $name:expr, $input:expr, $expected:expr) => {
+            paste::paste! {
+                #[test]
+                fn [<test_parse_ $ast:snake _ $name>]() {
+                    let pairs = crate::parsing::Tx3Grammar::parse(<$ast>::RULE, $input).unwrap();
+                    let single_match = pairs.into_iter().next().unwrap();
+                    let result = <$ast>::parse(single_match).unwrap();
+
+                    assert_eq!(result, $expected);
+                }
+            }
+        };
+    }
+
+    input_to_ast_check!(
+        PlutusWitnessBlock,
+        "basic",
+        "plutus_witness {
+            version: 3,
+            script: 0xABCDEF,
+        }",
+        PlutusWitnessBlock {
+            fields: vec![
+                PlutusWitnessField::Version(DataExpr::Number(3), Span::DUMMY),
+                PlutusWitnessField::Script(
+                    DataExpr::HexString(HexStringLiteral::new("ABCDEF".to_string())),
+                    Span::DUMMY
+                )
+            ],
+            span: Span::DUMMY,
+        }
+    );
 }
