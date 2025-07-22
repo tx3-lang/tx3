5a18aaa0bbe858b5a9385cce2eb46f879a63a04a
Benjamin Martinez Picech <bm0130647@gmail.com>
2025-07-22 14:35:48 -0300

    remove old version function

--- crates/tx3-lang/src/lib.rs
+++ crates/tx3-lang/src/lib.rs
@@ -419,10 +419,6 @@ impl ProtoTx {
         let (ir, _) = bincode::decode_from_slice::<ir::Tx, _>(bytes, config)?;
         Ok(Self::from(ir))
     }
-
-    pub fn inputs(&self) -> impl Iterator<Item = (&String, &UtxoSet)> {
-        self.inputs.iter()
-    }
 }
 
 impl AsRef<ir::Tx> for ProtoTx {
