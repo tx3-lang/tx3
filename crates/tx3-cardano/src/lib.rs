ab2e0bf4a3ae7a6c7023939dc4b3c58090c14064
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-09-12 12:47:52 -0300

    feat(compiler): add timestamp field to ChainPoint

--- crates/tx3-cardano/src/lib.rs
+++ crates/tx3-cardano/src/lib.rs
@@ -46,6 +46,7 @@ pub type TxBody =
 pub struct ChainPoint {
     pub slot: u64,
     pub hash: Vec<u8>,
+    pub timestamp: u128,
 }
 
 pub struct Compiler {
@@ -109,6 +110,34 @@ impl tx3_lang::backend::Compiler for Compiler {
                 }]))
             }
             ir::CompilerOp::ComputeTipSlot => Ok(ir::Expression::Number(self.cursor.slot as i128)),
+            ir::CompilerOp::ComputeSlotsToUnixTime(x) => {
+                let slot_i128 = coercion::expr_into_number(&x)?;
+                if slot_i128 < 0 {
+                    return Err(tx3_lang::backend::Error::CoerceError(
+                        format!("{}", slot_i128),
+                        "positive slot number".to_string(),
+                    ));
+                }
+
+                let current_time = self.cursor.timestamp as i128;
+                let time_diff = slot_i128 - self.cursor.slot as i128;
+                let unix_time_millis = current_time + (time_diff * 1000);
+
+                Ok(ir::Expression::Number(unix_time_millis))
+            }
+            ir::CompilerOp::ComputeUnixTimeToSlots(x) => {
+                let unix_time_i128 = coercion::expr_into_number(&x)?;
+                if unix_time_i128 < 0 {
+                    return Err(tx3_lang::backend::Error::CoerceError(
+                        format!("{}", unix_time_i128),
+                        "positive unix timestamp".to_string(),
+                    ));
+                }
+                let unix_time = unix_time_i128 as u64;
+                let slot =
+                    compute_unix_time_to_slot(unix_time, self.cursor.slot, self.cursor.timestamp)?;
+                Ok(ir::Expression::Number(slot as i128))
+            }
         }
     }
 }
@@ -117,4 +146,17 @@ fn eval_size_fees(tx: &[u8], pparams: &PParams, extra_fees: Option<u64>) -> u64 
     tx.len() as u64 * pparams.min_fee_coefficient
         + pparams.min_fee_constant
         + extra_fees.unwrap_or(DEFAULT_EXTRA_FEES)
+}
+
+fn compute_unix_time_to_slot(
+    unix_time: u64,
+    current_tip_slot: u64,
+    tip_timestamp: u128,
+) -> Result<u64, tx3_lang::backend::Error> {
+    let current_time = tip_timestamp as i128;
+
+    let time_diff = (unix_time as i128 - current_time) / 1000;
+    let slot = (current_tip_slot as i128 + time_diff) as u64;
+
+    Ok(slot)
 }
