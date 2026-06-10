d0ecb5eb7bae2e468b070e5d0b5ce640c77dc3c9
Santiago Carmuega <santiago@carmuega.me>
2026-06-10 08:55:08 -0300

    feat: add multiply (*) operator (#339)

--- crates/tx3-lang/src/analyzing.rs
+++ crates/tx3-lang/src/analyzing.rs
@@ -608,6 +608,19 @@ impl Analyzable for SubOp {
     }
 }
 
+impl Analyzable for MulOp {
+    fn analyze(&mut self, parent: Option<Rc<Scope>>) -> AnalyzeReport {
+        let left = self.lhs.analyze(parent.clone());
+        let right = self.rhs.analyze(parent.clone());
+
+        left + right
+    }
+
+    fn is_resolved(&self) -> bool {
+        self.lhs.is_resolved() && self.rhs.is_resolved()
+    }
+}
+
 impl Analyzable for NegateOp {
     fn analyze(&mut self, parent: Option<Rc<Scope>>) -> AnalyzeReport {
         self.operand.analyze(parent)
@@ -753,6 +766,7 @@ impl Analyzable for DataExpr {
             DataExpr::Identifier(x) => x.analyze(parent),
             DataExpr::AddOp(x) => x.analyze(parent),
             DataExpr::SubOp(x) => x.analyze(parent),
+            DataExpr::MulOp(x) => x.analyze(parent),
             DataExpr::NegateOp(x) => x.analyze(parent),
             DataExpr::PropertyOp(x) => x.analyze(parent),
             DataExpr::AnyAssetConstructor(x) => x.analyze(parent),
@@ -770,6 +784,7 @@ impl Analyzable for DataExpr {
             DataExpr::Identifier(x) => x.is_resolved(),
             DataExpr::AddOp(x) => x.is_resolved(),
             DataExpr::SubOp(x) => x.is_resolved(),
+            DataExpr::MulOp(x) => x.is_resolved(),
             DataExpr::NegateOp(x) => x.is_resolved(),
             DataExpr::PropertyOp(x) => x.is_resolved(),
             DataExpr::AnyAssetConstructor(x) => x.is_resolved(),
